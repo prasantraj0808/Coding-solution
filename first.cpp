d3fda1f103464bc74f4fa8907654f0faa621a827
Prasant <prasantraj0808@gmail.com>
2022-02-23 12:54:41 +0530

    stl using sort function

--- first.cpp
+++ first.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
    vector<int>v{2,3,1,7,9,12};
-   sort(v.begin(),v.end(),greater<int>());
+   sort(v.begin(),v.end());
    for(auto x:v)
    {
        cout<<x<<" ";
