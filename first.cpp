75d121ae2e9ea0c91bc08a70b48da8014d32a3ea
Prasant <prasantraj0808@gmail.com>
2022-02-24 18:09:43 +0530

    stl-unordered map

--- first.cpp
+++ first.cpp
@@ -1,42 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Food{
- public:
- string fname;
- int id;
- Food(string f,int i)
- {
-     fname=f;
-     id=i;
- }
- bool operator<(const Food &f)const{
-    if(this->fname.length()<f.fname.length())
-    {
-        return true;
-    }
-    else if(this->fname.length()==f.fname.length())
-    {
-        return this->id<f.id;
-    }
-    else{
-        return false;
-    }
- }
-};
+
 int main()
 {
     
-    Food f1("dosa",1),f2("maggi",2),f3("coffee",4),f4("pizza",3);
-        set<Food>s;
-        s.insert(f1);
-        s.insert(f2);
-        s.insert(f3);
-        s.insert(f4);
-        for(auto x:s)
-        {
-            cout<<x.fname<<" "<<x.id<<endl;
-        }
+    unordered_map<int,int>mp;
+    mp[1]=100;
+    mp.insert({4,299});
+    mp.insert({6,567});
+    if(mp.find(6)!=mp.end())
+    {
+        cout<<"element exist"<<endl;
+       cout<< mp.find(6)->second;
 
+    }
+    else
+    {
+        cout<<"element does not exist";
+    }
+    cout<<endl<<mp.size()<<endl;
+    mp.erase(mp.find(6));
+    for(auto x:mp)
+    {
+        cout<<x.first<<" "<<x.second<<endl;
+    }
     return 0;
 } 
