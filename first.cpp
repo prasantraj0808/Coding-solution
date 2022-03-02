ab1b009744f5b5a7499efb052557231013c56a23
Prasant <prasantraj0808@gmail.com>
2022-03-02 19:37:05 +0530

    two numbers with odd occurences gfg -bit

--- first.cpp
+++ first.cpp
@@ -1,74 +1,77 @@
-
-//https://practice.geeksforgeeks.org/problems/zero-sum-subarrays1825/1
+//https://practice.geeksforgeeks.org/problems/two-numbers-with-odd-occurrences5846/0/?category[]=Operators&page=1&query=category[]Operatorspage1
 // { Driver Code Starts
-//Initial function template for C++
-
-#include<bits/stdc++.h>
+//Initial Template for C++
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long
 
  // } Driver Code Ends
-//User function template for C++
-
+//User function Template for C++
 class Solution{
     public:
-    //Function to count subarrays with sum equal to 0.
-    ll findSubarray(vector<ll> arr, int n ) {
-        //code here
-        vector<ll>psum;
-        psum.push_back(arr[0]);
-        for(int j=1;j<n;j++)
+    vector<int> twoOddNum(int Arr[], int N)  
+    {
+        // code here
+        int xorofelements=0;
+        for(int i=0;i<N;i++)
         {
-            psum.push_back(arr[j]+psum[j-1]);
+            xorofelements^=Arr[i];
         }
-        ll count=0;
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++)
+        int positionofset=0;
+        while(xorofelements)
         {
-            if(psum[i]==0 && arr[i]==0)
+            if(xorofelements & 1)
             {
-                mp[0]++;
-                count+=mp[0];
-            }
-            else if(psum[i]==0 )
-            {
-                mp[0]++;
-                count+=mp[0];
-            }
-            else if(arr[i]==0  && mp[psum[i]]==0)
-            {
-                 mp[psum[i]]++;
-                count+=1;
+                break;
             }
             else
             {
-                mp[psum[i]]++;
-                if(mp[psum[i]]>1)
-                count+=mp[psum[i]]-1;
+                positionofset++;
+                xorofelements>>=1;
             }
-            
-            
         }
-        return count;
+        int mask=1<<positionofset;
+        int firstset=0,secondset=0;
+        for(int i=0;i<N;i++)
+        {
+            if(Arr[i] & mask)
+            firstset^=Arr[i];
+            else
+            secondset^=Arr[i];
+        }
+        vector<int>res;
+        if(firstset>=secondset)
+        {
+        res.push_back(firstset);
+        res.push_back(secondset);
+        }
+        else
+        {
+             
+        res.push_back(secondset);
+        res.push_back(firstset);
+        
+        }
+        return res;
+        
     }
 };
 
 // { Driver Code Starts.
 int main()
- {
+{
     int t;
     cin>>t;
     while(t--)
     {
-        int n;
-        cin>>n; //input size of array
-       
-        vector<ll> arr(n,0);
-        
-        for(int i=0;i<n;i++)
-            cin>>arr[i]; //input array elements
+        int N;
+        cin>>N;
+        int Arr[N];
+        for(int i = 0;i < N;i++)
+        cin>>Arr[i];
         Solution ob;
-        cout << ob.findSubarray(arr,n) << "\n";
+        vector<int>ans=ob.twoOddNum(Arr,N);
+        for(int i=0;i<ans.size();i++)cout<<ans[i]<<" ";
+        cout<<endl;
     }
-	return 0;
+    return 0;
 }  // } Driver Code Ends
