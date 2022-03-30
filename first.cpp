4b3490666b8fe55a03d51b6c1190d974e994a5b1
Prasant <prasantraj0808@gmail.com>
2022-03-30 17:16:50 +0530

    Kadanes algorithm

--- first.cpp
+++ first.cpp
@@ -1,54 +1,63 @@
-//https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays5135/1?msclkid=913970adaf7411ecbc53a48d0d85394b#
 // { Driver Code Starts
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
+
  // } Driver Code Ends
-//User function template for C++
 class Solution{
-public:
-	void merge(int arr1[], int arr2[], int n, int m) {
-	    // code here
-	    int temp;
-	    for(int i=n-1,j=0;i>=0 && j<m;i--,j++)
-	    {
-	        if(arr1[i]>arr2[j])
-	        {
-	        temp=arr1[i];
-	        arr1[i]=arr2[j];
-	        arr2[j]=temp;
-	        }
-	        else
-	        break;
-	    }
-	    sort(arr1,arr1+n);
-	    sort(arr2,arr2+m);
-	}
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        long long sum=0,global_max=INT_MIN,flag=0,maxi=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+            if(maxi<arr[i])
+            maxi=arr[i];
+            if(sum<0)
+            sum=0;
+            else
+            {
+                if(sum>global_max)
+                {
+                    global_max=sum;
+                    flag=1;
+                }
+            }
+        }
+        if(flag==0)
+        {
+            global_max=maxi;
+        }
+        return global_max;
+        
+    }
 };
 
 // { Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m, i;
-        cin >> n >> m;
-        int arr1[n], arr2[m];
-        for (i = 0; i < n; i++) {
-            cin >> arr1[i];
-        }
-        for (i = 0; i < m; i++) {
-            cin >> arr2[i];
-        }
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
         Solution ob;
-        ob.merge(arr1, arr2, n, m);
-        for (i = 0; i < n; i++) {
-            cout << arr1[i] << " ";
-        }
-        for (i = 0; i < m; i++) {
-            cout << arr2[i] << " ";
-        }
-        cout << "\n";
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
     }
-    return 0;
-}  // } Driver Code Ends
+}
+  // } Driver Code Ends
