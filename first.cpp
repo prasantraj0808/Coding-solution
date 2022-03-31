5052a6805d72e163dd6901891e2b3cae6ca9adf0
Prasant <prasantraj0808@gmail.com>
2022-04-01 00:24:16 +0530

    count pairs with given sum

--- first.cpp
+++ first.cpp
@@ -1,20 +1,50 @@
+//https://practice.geeksforgeeks.org/problems/count-pairs-with-given-sum5022/1#
+// { Driver Code Starts
+//Initial template for C++
 
-//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
 public:
-    int maxProfit(vector<int>& prices) {
-        int minimal=INT_MAX,maxprofit=0,currentprofit;
-        for(auto x:prices)
+    int getPairsCount(int arr[], int n, int k) {
+        unordered_map<int,int>mp;
+        int twicecount=0,count;
+        for(int i=0;i<n;i++)
         {
-            if(minimal>x)
-            {
-                minimal=x;
-               
-            }
-             currentprofit=x-minimal;
-                if(currentprofit>maxprofit)
-                    maxprofit=currentprofit;
+            mp[arr[i]]++;
         }
-        return maxprofit;
+        for(int i=0;i<n;i++)
+        {
+           twicecount+=mp[k-arr[i]];
+           if(arr[i]==k-arr[i])
+           twicecount--;
+        }
+        // code here
+        count=twicecount/2;
+        return count;
     }
 };
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getPairsCount(arr, n, k);
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}  // } Driver Code Ends
