a191ac8a74f5dafee97c81765eb2244520a63966
Prasant <prasantraj0808@gmail.com>
2022-04-01 17:50:48 +0530

    maximum product subaray

--- first.cpp
+++ first.cpp
@@ -1,64 +1,78 @@
-//https://practice.geeksforgeeks.org/problems/subarray-with-0-sum-1587115621/1
-// { Driver Code Starts
-// A C++ program to find if there is a zero sum
-// subarray
+
+//https://practice.geeksforgeeks.org/problems/maximum-product-subarray3604/1// { Driver Code Starts
 #include <bits/stdc++.h>
+
 using namespace std;
 
-
-
  // } Driver Code Ends
+//User function template for C++
 class Solution{
-    public:
-    //Complete this function
-    //Function to check whether there is a subarray present with 0-sum or not.
-    bool subArrayExists(int arr[], int n)
-    {
-        //Your code here
-        bool ans;
-        unordered_map<int,int>mp;
-        mp[arr[0]]++;
-        for(int i=1;i<n;i++)
+public:
+
+        void swap(long long &x,long long &y)
         {
-            arr[i]=arr[i]+arr[i-1];
-            if(arr[i]==0)
-            {
-                ans=true;
-                return ans;
-            }
-            mp[arr[i]]++;
+            long long temp;
+            temp=x;
+            x=y;
+            y=temp;
         }
-        for(int i=0;i<n;i++)
+        long long max(long long a,long long b)
         {
-            if(mp[arr[i]]>1)
-            {
-                ans=true;
-                break;
-                
-            }
+            if(a>=b)
+            return a;
+            else
+            return b;
         }
-        return ans;
-    }
+          long long min(long long a,long long b)
+        {
+            if(a>=b)
+            return b;
+            else
+            return a;
+        }
+
+	// Function to find maximum product subarray
+	long long maxProduct(vector<int> arr, int n) {
+	    // code here
+	   long long ma=arr[0],mi=arr[0],ans=arr[0];
+	   for(int i=1;i<arr.size();i++)
+	   {
+	       if(arr[i]>=0)
+	       {
+	       ma=max(arr[i],ma*arr[i]);
+	       mi=min(arr[i],mi*arr[i]);
+	       }
+	       else
+	       {
+	           swap(ma,mi);
+	           ma=max(arr[i],ma*arr[i]);
+	           mi=min(arr[i],mi*arr[i]);
+	       }
+	       if(ans<ma)
+	       {
+	           ans=ma;
+	       }
+	   }
+	   return ans;
+	    
+	}
 };
 
 // { Driver Code Starts.
-// Driver code
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int n;
-	    cin>>n;
-	    int arr[n];
-	    for(int i=0;i<n;i++)
-	    cin>>arr[i];
-	    Solution obj;
-	    	if (obj.subArrayExists(arr, n))
-		cout << "Yes\n";
-	else
-		cout << "No\n";
-	}
-	return 0;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.maxProduct(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
 }  // } Driver Code Ends
