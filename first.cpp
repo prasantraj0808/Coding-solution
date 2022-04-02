f300d09dee458c847d60763a36cdee61ff3367d3
Prasant <prasantraj0808@gmail.com>
2022-04-02 14:50:51 +0530

    bipartite graph test

--- first.cpp
+++ first.cpp
@@ -1,89 +1,35 @@
-#include<bits/stdc++.h> 
+bool dfs(int v,int c) 
+
+{ 
+
+visited[v]=1; 
+
+col[v]=c; 
+
+for(auto child: adj[v]) 
+
+{ 
+
+if(visited[child]==0) 
+
+{ 
+
+if(dfs(child,c^1)==false) 
+
+return false; 
+
+} 
+
+else 
+
+if(col[v]==col[child]) 
+
+return false;  
 
  
 
-using namespace std; 
-
- 
-
-vector<vector <int>> adj; 
-
-vector<int> dist; 
-
-vector<int> visited; 
-
- 
-
-void dfs(int src,int dista) 
-
-{ 
-
-visited[src]=1; 
-
-dist[src]=dista; 
-
-for(auto x:adj[src]) 
-
-{ 
-
-if(visited[x]==0) 
-
-{ 
-
-dfs(x,dista+1); 
-
 } 
 
-} 
-
-} 
-
-int main() 
-
-{ 
-
-int v,e; 
-
-cout<<"enter the no. of vertices and edges"<<endl; 
-
-cin>>v>>e; 
-
-adj.resize(v+1); 
-
-int a,b; 
-
-cout<<"enter the corresponding connected vertices"<<endl; 
-
-for(int i=0;i<e;i++) 
-
-{ 
-
-cin>>a>>b; 
-
-adj[a].push_back(b); 
-
-adj[b].push_back(a); 
-
-} 
-
-int src=1; 
-
-visited.resize(v+1,0); 
-
-dist.resize(v+1); 
-
-dfs(src,0); 
-
-for(int i=1;i<=v;i++) 
-
-{ 
-
-cout<<dist[i]<<"  "; 
-
-} 
-
- 
-
-return 0; 
+return true; 
 
 } 
