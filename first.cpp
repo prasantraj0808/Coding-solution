19f49cba07d07f90b548a80f9a85658d0aa1858e
Prasant <prasantraj0808@gmail.com>
2022-04-02 13:45:25 +0530

    single source shortest path in tree

--- first.cpp
+++ first.cpp
@@ -1,22 +1,37 @@
 #include<bits/stdc++.h> 
 
+ 
+
 using namespace std; 
 
+ 
+
 vector<vector <int>> adj; 
+
+vector<int> dist; 
+
 vector<int> visited; 
-void dfs(int src) 
+
+ 
+
+void dfs(int src,int dista) 
 
 { 
 
 visited[src]=1; 
-//cout<<src<<"--->";
 
-for(auto child:adj[src]) 
+dist[src]=dista; 
 
-{
-if(visited[child]==0) 
+for(auto x:adj[src]) 
+
 { 
-dfs(child); 
+
+if(visited[x]==0) 
+
+{ 
+
+dfs(x,dista+1); 
+
 } 
 
 } 
@@ -46,22 +61,29 @@ for(int i=0;i<e;i++)
 cin>>a>>b; 
 
 adj[a].push_back(b); 
+
 adj[b].push_back(a); 
 
 } 
+
 int src=1; 
 
 visited.resize(v+1,0); 
-int noofconnectedcomponent;
-for(int i=1;i<=v;i++)
-{
-    if(visited[i]==0)
-    {
-        dfs(i);
-        noofconnectedcomponent++;
-    }
-}
-cout<<"no of connected component is"<<noofconnectedcomponent; 
+
+dist.resize(v+1); 
+
+dfs(src,0); 
+
+for(int i=1;i<=v;i++) 
+
+{ 
+
+cout<<dist[i]<<"  "; 
+
+} 
+
+ 
+
 return 0; 
 
 } 
