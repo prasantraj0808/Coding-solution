cf998ca200440ca1113850ea47a7b86be4da6174
Prasant <prasantraj0808@gmail.com>
2022-04-02 12:47:03 +0530

    dfs in graph

--- first.cpp
+++ first.cpp
@@ -1,61 +1,58 @@
-//https://practice.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1
-// { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h> 
 
+using namespace std; 
 
- // } Driver Code Ends
-class Solution{
-    public:
-    //Function to find if there exists a triplet in the 
-    //array A[] which sums up to X.
-    bool find3Numbers(int A[], int n, int X)
-    {
-        //Your Code Here
-        sort(A,A+n);
-        int l,r,ans=0;
-        for(int i=0;i<n-2;i++)
-        {
-            l=i+1;
-            r=n-1;
-            while(l<r)
-            {
-                if(A[i]+A[l]+A[r]==X)
-                {
-                    ans=1;
-                    break;
-                }
-                else if(A[i]+A[l]+A[r]<X)
-                l++;
-                else
-                r--;
-            }
-            if(ans==1)
-            break;
-        }
-        if(ans==1)
-        return 1;
-        else
-        return 0;
-    }
+vector<vector <int>> adj; 
+vector<int> visited; 
+void dfs(int src) 
 
-};
+{ 
 
-// { Driver Code Starts.
+visited[src]=1; 
+cout<<src<<"--->";
 
-int main()
+for(auto child:adj[src]) 
+
 {
-	int T;
-	cin>>T;
-	while(T--)
-	{
-		int n,X;
-		cin>>n>>X;
-		int i,A[n];
-		for(i=0;i<n;i++)
-			cin>>A[i];
-		Solution ob;
-        cout <<  ob.find3Numbers(A, n, X) << endl;
-    }
-}
-  // } Driver Code Ends
+if(visited[child]==0) 
+{ 
+dfs(child); 
+} 
+
+} 
+
+} 
+
+int main() 
+
+{ 
+
+int v,e; 
+
+cout<<"enter the no. of vertices and edges"<<endl; 
+
+cin>>v>>e; 
+
+adj.resize(v+1); 
+
+int a,b; 
+
+cout<<"enter the corresponding connected vertices"<<endl; 
+
+for(int i=0;i<e;i++) 
+
+{ 
+
+cin>>a>>b; 
+
+adj[a].push_back(b); 
+adj[b].push_back(a); 
+
+} 
+int src=1; 
+
+visited.resize(v+1,0); 
+dfs(src); 
+return 0; 
+
+} 
