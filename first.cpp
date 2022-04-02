bffc40e26ba1221893c53d494d5e85633bda8c21
Prasant <prasantraj0808@gmail.com>
2022-04-02 18:42:07 +0530

    In and Out time of nodes for finding articulation point and bridges

--- first.cpp
+++ first.cpp
@@ -4,29 +4,22 @@ using namespace std;
 
 vector<vector <int>> adj; 
 vector<int> visited; 
-bool dfs(int src) 
+vector<int> In;
+vector<int> Out;
+int timer=1;
+void dfs(int src) 
 
 { 
 
 visited[src]=1; 
 //cout<<src<<"--->";
+    In[src]=timer++;
+    for(auto child :adj[src])
+    {
+        if(visited[child]==0)
+        dfs(child);
+    }
 
-for(auto child:adj[src]) 
-
-{
-if(visited[child]==0) 
-{ 
-    if(dfs(child)==true)
-    return true;
-} 
-else{
-    if(visited[child]==1)
-    return true;
-}
-
-} 
-    visited[src]=2;
-    return false;
 } 
 
 int main() 
@@ -52,14 +45,16 @@ for(int i=0;i<e;i++)
 cin>>a>>b; 
 
 adj[a].push_back(b); 
-//adj[b].push_back(a); 
+adj[b].push_back(a); 
 
 } 
 int src=1; 
 
-visited.resize(v+1,0); 
-bool ans=dfs(src);
-cout<<ans; 
+visited.resize(v+1,0);
+In.resize(v+1);
+Out.resize(v+1); 
+dfs(src);
+ 
 return 0; 
 
 } 
