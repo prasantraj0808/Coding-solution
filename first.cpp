10d39e000d650d700d40a2747cec7480402521fe
Prasant <prasantraj0808@gmail.com>
2022-04-02 17:24:57 +0530

    cycle detection using dfs

--- first.cpp
+++ first.cpp
@@ -1,92 +1,63 @@
-//https://www.spoj.com/problems/BUGLIFE/
-#include<bits/stdc++.h>
+#include<bits/stdc++.h> 
 
-using namespace std;
+using namespace std; 
 
-vector <int> visited;
-vector <int> col;
-vector<vector<int>> adj;
- 
-bool dfs(int v,int c)
+vector<vector <int>> adj; 
+vector<int> visited; 
+bool dfs(int src,int par) 
+
+{ 
+
+visited[src]=1; 
+//cout<<src<<"--->";
+
+for(auto child:adj[src]) 
+
 {
-	visited[v]=1;
-	col[v]=c;
-	for(auto child: adj[v])
-	{
-		if(visited[child]==0)
-		{
-			if(dfs(child,c^1)==false)
-			return false;
-		}
-		else
-		if(col[v]==col[child])
-		return false; 
-		
-	}
-	return true;
-}
-int main()
-{
-	int nos;
-	cin>>nos;
- bool ans;
- int nov,noe;
- for(int j=1;j<=nos;j++)
- {
- 
- //cout<<"enter the no. of vertices"<<endl;
- cin>>nov;
- //cout<<"enter the no. of edges"<<endl;
- cin>>noe;
- 
- visited.resize(nov+1,0);
- 	col.clear();
- col.resize(nov+1);
-adj.resize(nov+1);
-
- 		for(int i=1;i<=nov;i++)
- 	{
- 		adj[i].clear();
- 		visited[i]=0;
-	}
-
-int a,b;
-for(int i=0;i<noe;i++)
-{
-	cin>>a>>b;
-	adj[a].push_back(b);
-	adj[b].push_back(a);
+if(visited[child]==0) 
+{ 
+    if(dfs(child,src)==true)
+    return true;
 } 
- 
- ans=dfs(1,0);
- int k=1;
- if(ans==0)
- {
-    cout<<"Scenario #"<<j<<":"<<endl<<"Suspicious bugs found!"<<endl;
- }
- else
- {
-      	while(k<=nov)
-      	{
-      		if(visited[k]==0)
-      	     ans=dfs(k,0);
-			   if(ans==0)
-                 {
- 	                cout<<"Scenario #"<<j<<":"<<endl<<"Suspicious bugs found!"<<endl;
- 	                break;
-                 }	
-                 k++;
-		}
- }
- 
- if(ans==1)
-    cout<<"Scenario #"<<j<<":"<<endl<<"No suspicious bugs found!"<<endl;
-    
- k=1;
-// cout<<ans;
-ans=-1;
-	
+else{
+    if(child!=par)
+    return true;
 }
-	
-	return 0;
-}
+
+} 
+    return false;
+} 
+
+int main() 
+
+{ 
+
+int v,e; 
+
+cout<<"enter the no. of vertices and edges"<<endl; 
+
+cin>>v>>e; 
+
+adj.resize(v+1); 
+
+int a,b; 
+
+cout<<"enter the corresponding connected vertices"<<endl; 
+
+for(int i=0;i<e;i++) 
+
+{ 
+
+cin>>a>>b; 
+
+adj[a].push_back(b); 
+adj[b].push_back(a); 
+
+} 
+int src=1; 
+
+visited.resize(v+1,0); 
+dfs(src,-1); 
+return 0; 
+
+} 
