4c689d8c0c1edd0732570e76228f5291ed08323b
Prasant <prasantraj0808@gmail.com>
2022-04-03 10:02:39 +0530

    Feasible relation

--- first.cpp
+++ first.cpp
@@ -1,69 +1,81 @@
-//https://www.spoj.com/problems/PPATH/
-#include<bits/stdc++.h>
-
+//https://www.hackerearth.com/practice/algorithms/graphs/depth-first-search/practice-problems/algorithm/feasible-relations/
+#include <bits/stdc++.h>
+ 
 using namespace std;
-
+ 
 vector<vector<int>> adj;
-vector<int>visited;
-vector<int>primes;
-vector<int>dist;
-queue<int>q;
-
-bool isprime(int n)
+vector<int> visited;
+//queue<int > q;
+//vector<int>dist;
+//vector<int>prime;
+//vector<int> visitedvalid;
+ 
+/*
+ 
+bool isprime(int a)
 {
-    for(int i=2;i*i<=n;i++)
-    {
-        if(n%i == 0)
-        return false;
-        
-      
-    }
-      return true;
+	for(int i=2;i*i<=a;i++)
+	{
+		if(a%i==0)
+		return false;
+		
+	}
+	return true;
 }
 bool isvalid(int a,int b)
 {
-    int count=0;
-    while(a>0)
-    {
-        if(a%10 != b%10)
-        count++;
-        a/=10;
-        b/=10;
+	int count=0;
+	while(b!=0)
+	{
+	if(a%10 != b%10)
+	count++;
+	a=a/10;
+	b=b/10;
     }
     if(count==1)
     return true;
-    else
-    return false;
+    else 
+	return false; 
+	
 }
+ 
 void buildgraph()
 {
-    for(int i=1000;i<=9999;i++)
-    {
-        if(isprime(i))
-        {
-            primes.push_back(i);
-        }
-    }
-    for(int i=0;i<primes.size();i++)
-    {
-        for(int j=i+1;j<primes.size();j++)
-        {
-            int a=primes[i];
-            int b=primes[j];
-            if(isvalid(a,b))
-            {
-                adj[a].push_back(b);
-                adj[b].push_back(a);
-            }
-        }
-    }
-
+	for(int i=1000;i<=9999;i++)
+      {
+         if(isprime(i))
+		 prime.push_back(i); 	
+	  }	
+ 
+	for(int i=0;i<prime.size();i++)
+	{
+		for(int j=i+1;j<prime.size();j++)
+		
+		{
+			int a=prime[i],b=prime[j];
+			
+			if(isvalid(a,b))
+			{
+				adj[a].push_back(b);
+				adj[b].push_back(a);
+			}
+		}
+	}
 }
-void  bfs(int v)
+ 
+*/
+ 
+ 
+int no_of_var,no_of_rel,curr_cc=0;
+vector<pair<int,int>> notequal;
+vector<int >cc;
+ 
+/*
+bool  bfs(int v,int b)
 {
     visited[v]=1;
     q.push(v);
-    dist[v]=0;
+   // dist[v]=0;
     int current=v;
     while(!q.empty())
     {
@@ -71,41 +83,174 @@ void  bfs(int v)
         q.pop();
         for(auto x:adj[current])
         {
+        	if(x==b)
+        	{
+			  while(!q.empty())
+			  q.pop();
+        	return true;
+            }
+			
             if(visited[x]==0)
             {
-                dist[x]=dist[current]+1;
+              //  dist[x]=dist[current]+1;
                 visited[x]=1;
                 q.push(x);
             }
         }
        
     }
+    //cout<<"bfs "<<endl;
 }
+ 
+*/
+ 
+void dfs(int v)
+{
+	visited[v]=1;
+	cc[v]=curr_cc;
+	for(auto x : adj[v])
+	{
+		if(visited[x]==0)
+		{
+			dfs(x);
+		}
+	}
+}
+ 
+ 
+/*
+bool valid(int a,int b,string r)
+{
+	if(r == "=")
+	{
+		if(visitedvalid[a]==1 && visitedvalid[b]==1)
+		{
+			if(bfs(a,b)==false)
+			{
+				for(int i=1;i<=no_of_var;i++)
+				{
+					visited[i]=0;
+				}
+				return false;
+			}
+			else
+			return true;
+		}
+		else
+		{
+			adj[b].push_back(a);
+			adj[a].push_back(b);
+			visitedvalid[a]=1;
+			visitedvalid[b]=1;
+			return true;
+		}
+	}
+	else
+	{
+		if(visitedvalid[a]==1 && visitedvalid[b]==1)
+		{
+			if(bfs(a,b)==true)
+			{
+				for(int i=0;i<no_of_var;i++)
+				{
+					visited[i]=0;
+				}
+				return false;
+			}
+			else
+			return true;
+		}
+		else
+		{
+			visitedvalid[a]=1;
+			visitedvalid[b]=1;
+			return true;
+		}
+	}
+}
+ 
+*/
+ 
 int main()
 {
-    int t,a,b;
-    cin>>t;
-    adj.resize(10000);
-    buildgraph();
-    while(t--)
-    {
-        cin>>a>>b;
-        dist.resize(10000,-1);
-        visited.resize(10000,0);
-        primes.resize(10000);
-        bfs(a);
-
-        if(dist[b]==-1)
-        cout<<"Impossible"<<endl;
-        else
-        cout<<dist[b]<<endl;
-        for(int i=1000;i<=9999;i++)
-        {
-            dist[i]=-1;
-            visited[i]=0;
-
-        }
-
-    }
+  /*
+   int ini,des;
+  // cout<<"enter the initial and the final prime no."<<endl;
+   cin>>ini>>des;
+   dist.resize(100007,-1);
+   buildgraph();
+   bfs(ini);
+   cout<<dist[des];
+ //  cout<<"hello";
+    */
+	
+	 int t;
+	 cin>>t;
+	 for(int i=0;i<t;i++)
+	 {
+	 //	cout<<"enter the no of variables"<<endl;
+	 	cin>>no_of_var;
+	 //	cout<<"enter the no of relations"<<endl;
+	 	cin>>no_of_rel;
+	 	visited.resize(no_of_var+1,0);
+	 //	visitedvalid.resize(no_of_var+1,0);
+	 	adj.resize(no_of_var+1);
+	 	cc.resize(no_of_var+1);
+	 	
+        int a,b;
+		string rel;
+		bool ans=true;
+		for(int j=0;j<no_of_rel;j++)
+		{
+	//		cout<<"enter a relation and b"<<endl;
+			cin>>a>>rel>>b;
+		     if(rel == "=")
+		     {
+		     	adj[a].push_back(b);
+		     	adj[b].push_back(a);
+			 }
+			 
+			 else
+			 {
+			 	notequal.push_back({a,b});
+			 }
+				
+			
+	    } 
+	    for(int k=1;k<=no_of_var;k++)
+	    {
+	    	if(visited[k]==0)
+	    	{
+	    		curr_cc++;
+	    		dfs(k);
+			}
+		}
+	    for(int k=0;k<notequal.size();k++)
+	    {
+	    	a=notequal[k].first;
+	    	b=notequal[k].second;
+	    	if(cc[a]==cc[b])
+	    	{
+	    		ans=false;
+	    		break;
+			}
+		}
+		if(ans==true)
+		cout<<"YES"<<endl;
+		else
+		cout<<"NO"<<endl;
+		for(int j=1;j<=no_of_var;j++)
+		{
+		  visited[j]=0;
+		  adj[j].clear();
+		  cc[j]=0;
+		  //visitedvalid[j]=0;
+		}
+	     notequal.clear();	
+		 curr_cc=0;
+	 }
+	    
+ 
     return 0;
 }
+Language: C++14
