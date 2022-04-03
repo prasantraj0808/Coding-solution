b0f0c1f25c6ef783642f6c5c4e48180585a4f72d
Prasant <prasantraj0808@gmail.com>
2022-04-03 22:24:31 +0530

    counting no of set bits

--- first.cpp
+++ first.cpp
@@ -1,92 +1,15 @@
-//https://www.spoj.com/problems/TOPOSORT/
-/*
-input
-
-9
-10
-1
-2
-3
-4
-1
-8
-2
-9
-2
-5
-4
-5
-4
-8
-5
-9
-5
-7
-6
-7
-
-output
-
-1-->3-->6-->2-->4-->5-->8-->9-->7-->
-
-*/
-
-#include<bits/stdc++.h>
-
-using namespace std;
-
-vector<vector<int>>adj;
-vector<int> res;
-vector<int>in;
-
-bool kahn(int n)
-{
-	priority_queue<int,vector<int>,greater<int>>q;
-	for(int i=1;i<=n;i++)
-	{
-		if(in[i]==0)
-		q.push(i);
-	}
-	while(!q.empty())
-	{
-		int curr=q.top();
-		q.pop();
-		res.push_back(curr);
-		for(auto node:adj[curr])
-		{
-			in[node]--;
-			if(in[node]==0)
-			q.push(node);
-		}
-	}
-	return res.size()==n;
-}
-int main()
-{
-	int n,m,x,y;
-	cin>>n>>m;
-	adj.resize(n+1);
-	in.resize(n+1,0);
-		//res.resize(n+1);
-	for(int i=1;i<=m;i++)
-	{
-		cin>>x>>y;
-		adj[x].push_back(y);
-		in[y]++;
-		
-	}
-	//res.clear();
-	
-	
-	bool ans=kahn(n);
-	if(ans==true)
-	{
-		for(auto node:res)
-		cout<<node<<" ";
-	}
-	else{
-		cout<<"Sandro fails.";
-	}
-	
-	return 0;
-}
+//https://leetcode.com/problems/number-of-1-bits/
+class Solution {
+public:
+    int hammingWeight(uint32_t n) 
+    {
+        unsigned int cnt=0;
+        while(n>0)
+        {
+          
+		     cnt++;  //another mthod is also there
+		     n=n & (n-1);
+        }
+        return cnt;
+    }
+};
