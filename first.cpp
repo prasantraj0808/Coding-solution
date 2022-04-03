b86496ba6debd4320ae6305048f3e44a86881234
Prasant <prasantraj0808@gmail.com>
2022-04-03 14:58:01 +0530

    submerging island

--- first.cpp
+++ first.cpp
@@ -1,9 +1,15 @@
-int n; // number of nodes
+//https://www.spoj.com/problems/SUBMERGE/
+#include<bits/stdc++.h>
+
+using namespace std;
+//int n; // number of nodes
 vector<vector<int>> adj; // adjacency list of graph
 
 vector<bool> visited;
 vector<int> tin, low;
-int timer;
+int timer=0;
+int ans=0;
+ set<int, greater<int> > s1;
 
 void dfs(int v, int p = -1) {
     visited[v] = true;
@@ -17,21 +23,63 @@ void dfs(int v, int p = -1) {
             dfs(to, v);
             low[v] = min(low[v], low[to]);
             if (low[to] >= tin[v] && p!=-1)
-                IS_CUTPOINT(v);
+            s1.insert(v);
+                 //ans++;
+                // cout<<"vertex"<<v <<"is an articulation point"<<endl;
+                //IS_CUTPOINT(v);
+               
             ++children;
         }
     }
     if(p == -1 && children > 1)
-        IS_CUTPOINT(v);
+    s1.insert(v);
+    //ans++;
+    // cout<<"vertex"<<v <<"is an articulation point"<<endl;
+       // IS_CUTPOINT(v);
 }
 
-void find_cutpoints() {
-    timer = 0;
-    visited.assign(n, false);
-    tin.assign(n, -1);
-    low.assign(n, -1);
-    for (int i = 0; i < n; ++i) {
+int main() 
+{
+    //timer = 0;
+    
+    int n,e,a,b;
+   cin>>n>>e;
+    while(n!=0 || e!=0)
+    {
+    
+    adj.resize(n+1);
+    for(int j=0;j<e;j++)
+    {
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    
+    
+    visited.resize(n+1, false);
+    tin.resize(n+1, -1);
+    low.resize(n+1, -1);
+    
+    for (int i = 1; i <= n; ++i)
+    {
         if (!visited[i])
             dfs (i);
     }
+    
+    //dfs(1);
+    cout<<s1.size()<<endl;
+    for(int k=1;k<=n;k++)
+    {
+    	adj[k].clear();
+    	tin[k]=-1;
+    	low[k]=-1;
+    	visited[k]=false;
+	}
+	tin.clear();
+	low.clear();
+	s1.clear();
+    timer=0;
+    cin>>n>>e;
+    }
+    return 0;
 }
