439196d553518eb9a252fcc94174a75c7b263926
Prasant <prasantraj0808@gmail.com>
2022-04-03 17:32:40 +0530

    kahns algorithm for topological sort

--- first.cpp
+++ first.cpp
@@ -1,85 +1,82 @@
-//https://www.spoj.com/problems/SUBMERGE/
+/*
+input
+
+9
+10
+1
+2
+3
+4
+1
+8
+2
+9
+2
+5
+4
+5
+4
+8
+5
+9
+5
+7
+6
+7
+
+output
+
+1-->3-->6-->2-->4-->5-->8-->9-->7-->
+
+*/
+
 #include<bits/stdc++.h>
 
 using namespace std;
-//int n; // number of nodes
-vector<vector<int>> adj; // adjacency list of graph
 
-vector<bool> visited;
-vector<int> tin, low;
-int timer=0;
-int ans=0;
- set<int, greater<int> > s1;
+vector<vector<int>>adj;
+vector<int> res;
+vector<int>in;
 
-void dfs(int v, int p = -1) {
-    visited[v] = true;
-    tin[v] = low[v] = timer++;
-    int children=0;
-    for (int to : adj[v]) {
-        if (to == p) continue;
-        if (visited[to]) {
-            low[v] = min(low[v], tin[to]);
-        } else {
-            dfs(to, v);
-            low[v] = min(low[v], low[to]);
-            if (low[to] >= tin[v] && p!=-1)
-            s1.insert(v);
-                 //ans++;
-                // cout<<"vertex"<<v <<"is an articulation point"<<endl;
-                //IS_CUTPOINT(v);
-               
-            ++children;
-        }
-    }
-    if(p == -1 && children > 1)
-    s1.insert(v);
-    //ans++;
-    // cout<<"vertex"<<v <<"is an articulation point"<<endl;
-       // IS_CUTPOINT(v);
-}
-
-int main() 
+void kahn(int n)
 {
-    //timer = 0;
-    
-    int n,e,a,b;
-   cin>>n>>e;
-    while(n!=0 || e!=0)
-    {
-    
-    adj.resize(n+1);
-    for(int j=0;j<e;j++)
-    {
-        cin>>a>>b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    
-    
-    visited.resize(n+1, false);
-    tin.resize(n+1, -1);
-    low.resize(n+1, -1);
-    
-    for (int i = 1; i <= n; ++i)
-    {
-        if (!visited[i])
-            dfs (i);
-    }
-    
-    //dfs(1);
-    cout<<s1.size()<<endl;
-    for(int k=1;k<=n;k++)
-    {
-    	adj[k].clear();
-    	tin[k]=-1;
-    	low[k]=-1;
-    	visited[k]=false;
+	queue<int>q;
+	for(int i=1;i<=n;i++)
+	{
+		if(in[i]==0)
+		q.push(i);
 	}
-	tin.clear();
-	low.clear();
-	s1.clear();
-    timer=0;
-    cin>>n>>e;
-    }
-    return 0;
+	while(!q.empty())
+	{
+		int curr=q.front();
+		q.pop();
+		res.push_back(curr);
+		for(auto node:adj[curr])
+		{
+			in[node]--;
+			if(in[node]==0)
+			q.push(node);
+		}
+	}
+	for(auto node:res){
+		cout<<node<<"-->";
+	}
+}
+int main()
+{
+	int n,m,x,y;
+	cin>>n>>m;
+	adj.resize(n+1);
+	in.resize(n+1);
+		//res.resize(n+1);
+	for(int i=1;i<=m;i++)
+	{
+		cin>>x>>y;
+		adj[x].push_back(y);
+		in[y]++;
+		
+	}
+	//res.clear();
+	kahn(n);
+	return 0;
 }
