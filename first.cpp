7573d4d7bfcacbecabdbfc4b73057c3425b44408
Prasant <prasantraj0808@gmail.com>
2022-04-07 23:37:37 +0530

    diameter o binary tree

--- first.cpp
+++ first.cpp
@@ -1,135 +1,138 @@
-//https://practice.geeksforgeeks.org/problems/height-of-binary-tree/1
+//https://practice.geeksforgeeks.org/problems/diameter-of-binary-tree/1
 // { Driver Code Starts
-//Initial template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node
-{
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
     int data;
-    struct Node *left;
-    struct Node *right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-}; 
-
-// Function to Build Tree
-Node* buildTree(string str)
-{   
+    struct Node* left;
+    struct Node* right;
+};
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+Node* buildTree(string str) {
     // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
     // string after spliting by space
     vector<string> ip;
-    
+
     istringstream iss(str);
-    for(string str; iss >> str; )
-        ip.push_back(str);
-        
+    for (string str; iss >> str;) ip.push_back(str);
+
     // Create the root of the tree
-    Node *root = new Node(stoi(ip[0]));
-        
+    Node* root = newNode(stoi(ip[0]));
+
     // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
+    while (!queue.empty() && i < ip.size()) {
+
         // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
-        // If the left child is not null
-        if(currVal != "N") {
 
-            // Create the left child for the current Node
-            currNode->left = new Node(stoi(currVal));
-                
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
-        if(i >= ip.size())
-            break;
+        if (i >= ip.size()) break;
         currVal = ip[i];
-            
+
         // If the right child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the right child for the current node
-            currNode->right = new Node(stoi(currVal));
-                
+            currNode->right = newNode(stoi(currVal));
+
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
+
     return root;
 }
 
- // } Driver Code Ends
-//User function template for C++
 
-/*
+ // } Driver Code Ends
+/* Tree node structure  used in the program
+
 struct Node
 {
     int data;
     struct Node* left;
     struct Node* right;
-    
+
     Node(int x){
         data = x;
         left = right = NULL;
     }
-};
-*/
-class Solution{
-    public:
-    //Function to find the height of a binary tree.
-    int height(struct Node* node){
-        // code here 
-        if(node==NULL)
-        {
-            return 0;
-        }
-        else
-        {
-            int ldepth=height(node->left);
-            int rdepth=height(node->right);
-             if(ldepth>rdepth)
-             return ldepth+1;
-             else
-             return rdepth+1;
-        }
+}; */
+
+class Solution {
+  public:
+    // Function to return the diameter of a Binary Tree.
+    int maxi;
+    int func(Node* root)
+    {
+        
+             
+    if(root==NULL)
+    return 0;
+    
+    int lh=func(root->left);
+    int rh=func(root->right);
+    maxi=max(maxi,lh+rh+1);
+    
+    return (max(lh,rh)+1);
     }
+    int diameter(Node* root) {
+        // Your code here
+         maxi=INT_MIN;
+        int x=func(root);
+        return maxi;
+   
+    }
+    
+    
 };
 
 // { Driver Code Starts.
-int main()
-{
+
+/* Driver program to test size function*/
+int main() {
     int t;
-	scanf("%d ",&t);
-    while(t--)
-    {
-        string treeString;
-		getline(cin,treeString);
-		Node* root = buildTree(treeString);
+    scanf("%d\n", &t);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
         Solution ob;
-		cout<<ob.height(root)<<endl;
+        cout << ob.diameter(root) << endl;
     }
     return 0;
-}  // } Driver Code Ends
+}
+  // } Driver Code Ends
