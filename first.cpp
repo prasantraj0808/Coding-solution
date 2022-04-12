264a0cbe261d88cc59eba8c04efce804d8294a64
Prasant <prasantraj0808@gmail.com>
2022-04-13 00:41:26 +0530

    q 45 search element in a matrix

--- first.cpp
+++ first.cpp
@@ -1,141 +1,61 @@
-//https://practice.geeksforgeeks.org/problems/sum-tree/1
-// { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node
-{
-    int data;
-    struct Node *left;
-    struct Node *right;
-};
-// Utility function to create a new Tree Node
-Node* newNode(int val)
-{
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-    
-    return temp;
-}
-// Function to Build Tree
-Node* buildTree(string str)
-{   
-    // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
-    // string after spliting by space
-    vector<string> ip;
-    
-    istringstream iss(str);
-    for(string str; iss >> str; )
-        ip.push_back(str);
-        
-    // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-        
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-        
-    // Starting from the second element
-    int i = 1;
-    while(!queue.empty() && i < ip.size()) {
+//
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int noofrows=matrix.size(),noofcolumn=matrix[0].size(),flag=-1;
+        bool ans=false;
+        int i;
+        for(i=0;i<noofrows;i++)
+        {
+            if(matrix[i][0]>target)
+            {
+                flag=1;
+                break;
+            }
+            else if(matrix[i][0]==target)
+            {
+                flag=2;
+                break;
+            }
+            else
+            {
+              flag=0;  
+            }
             
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-            
-        // Get the current node's value from the string
-        string currVal = ip[i];
-            
-        // If the left child is not null
-        if(currVal != "N") {
-                
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->left);
         }
+        if(flag==0  )
+        {
+            i--;
+            for(int j=0;j<noofcolumn;j++)
+            {
+                if(matrix[i][j]==target)
+                {
+                ans=true;
+                    break;
+                }
+            }
             
-        // For the right child
-        i++;
-        if(i >= ip.size())
-            break;
-        currVal = ip[i];
-            
-        // If the right child is not null
-        if(currVal != "N") {
-                
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->right);
         }
-        i++;
-    }
-    
-    return root;
-}
-
-
- // } Driver Code Ends
-/*  Tree node
-struct Node
-{
-    int data;
-    Node* left, * right;
-}; */
-
-// Should return true if tree is Sum Tree, else false
-class Solution
-{
-    public:
-    int ans=1;
-    int func(Node* root)
-    {
-        if(root==NULL)
-        return 0;
+        else if(flag==1 && i>=1)
+        {
+            i--;
+              for(int j=0;j<noofcolumn;j++)
+            {
+                if(matrix[i][j]==target)
+                {
+                ans=true;
+                    break;
+                }
+            }
+            
+        }
+        else
+            if(flag==2)
+            ans=true;
         
-            int l= func(root->left);
+        return ans;
+            
+            
         
-             int r=func(root->right);
-             if(l!=0 && r!=0)
-             {
-               if(l+r != root->data)
-                 ans=0;
-             }
-            return l+r+root->data;
-            ans=0;
-         
-    }
-    bool isSumTree(Node* root)
-    {
-         // Your code here
-         int j=func(root);
-         return ans;
     }
 };
-
-// { Driver Code Starts.
-
-int main()
-{
-
-    int t;
-	scanf("%d ",&t);
-    while(t--)
-    {
-        string s;
-		getline(cin,s);
-        Node* root = buildTree(s);
-        Solution ob;
-        cout <<ob.isSumTree(root) << endl;
-    }
-    return 1;
-}  // } Driver Code Ends
