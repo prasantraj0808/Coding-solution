b169b8521ec89da1b2d44a6f6abb451d36b49a83
Prasant <prasantraj0808@gmail.com>
2022-04-13 19:10:34 +0530

    nearest smallest element on right

--- first.cpp
+++ first.cpp
@@ -1,53 +1,65 @@
-//https://practice.geeksforgeeks.org/problems/smallest-number-on-left3403/1/
+//https://practice.geeksforgeeks.org/problems/fab3dbbdce746976ba35c7b9b24afde40eae5a04/1/
 // { Driver Code Starts
-// Initial Template for C++
+//Initial Template for C++
 
-#include <bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h> 
+using namespace std; 
+
 
  // } Driver Code Ends
-// User function Template for C++
+//User function Template for C++
 
 class Solution{
-public:
-    vector<int> leftSmaller(int n, int a[]){
-        // code here
-        vector<int> ans;
-        stack<int>st;
-        for(int i=0;i<n;i++)
-        {
-            while(!st.empty() && st.top()>=a[i])
-          {
-            st.pop();
-          } 
-          if(st.empty())
-            ans.push_back(-1);
-            else
-            ans.push_back(st.top());
-            st.push(a[i]);
+    
+    public:
+    vector<int> help_classmate(vector<int> arr, int n) 
+    { 
+        // Your code goes here
+        vector<int > ans;
+        stack<int> st;
         
+        for(int i=n-1;i>=0;i--)
+        { 
+            while(!st.empty() && st.top()>=arr[i])
+            {
+                st.pop();
+            }
+            if(st.empty())
+              ans.push_back(-1);
+              else
+              ans.push_back(st.top());
+              st.push(arr[i]);
+            
         }
+        reverse(ans.begin(),ans.end());
         return ans;
-    }
+    } 
 };
 
 // { Driver Code Starts.
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int a[n];
-        for(int i = 0;i < n;i++)
-            cin>>a[i];
-        
-        Solution ob;
-        vector<int> ans = ob.leftSmaller(n, a);
-        for(int i = 0;i < n;i++)
-            cout<<ans[i]<<" ";
-        cout<<endl;
-    }
-    return 0;
-}  // } Driver Code Ends
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<int> array(n);
+		for (int i = 0; i < n; ++i)
+		{
+			cin>>array[i];
+		}
+		Solution obj;
+		vector<int> result = obj.help_classmate(array,n);
+		for (int i = 0; i < n; ++i)
+		{
+			cout<<result[i]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0; 
+}
+
+  // } Driver Code Ends
