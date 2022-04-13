26b6331a6324f983299df9a3cbe3a7adb0757831
Prasant <prasantraj0808@gmail.com>
2022-04-13 19:00:42 +0530

    smallest number on left

--- first.cpp
+++ first.cpp
@@ -1,4 +1,4 @@
-//https://practice.geeksforgeeks.org/problems/sorted-matrix2333/1
+//https://practice.geeksforgeeks.org/problems/smallest-number-on-left3403/1/
 // { Driver Code Starts
 // Initial Template for C++
 
@@ -8,48 +8,46 @@ using namespace std;
  // } Driver Code Ends
 // User function Template for C++
 
-class Solution {
-  public:
-    vector<vector<int>> sortedMatrix(int N, vector<vector<int>> Mat) {
+class Solution{
+public:
+    vector<int> leftSmaller(int n, int a[]){
         // code here
-        vector<int> arrmat;
-        for(int i=0;i<N;i++)
+        vector<int> ans;
+        stack<int>st;
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<N;j++)
-            {
-                arrmat.push_back(Mat[i][j]);
-            }
+            while(!st.empty() && st.top()>=a[i])
+          {
+            st.pop();
+          } 
+          if(st.empty())
+            ans.push_back(-1);
+            else
+            ans.push_back(st.top());
+            st.push(a[i]);
+        
         }
-        sort(arrmat.begin(),arrmat.end());
-        int ptr=0;
-        for(int i=0;i<N;i++)
-        {
-            for(int j=0;j<N;j++)
-            {
-                Mat[i][j]=arrmat[ptr];
-                ptr++;
-            }
-        }
-        return Mat;
+        return ans;
     }
 };
 
 // { Driver Code Starts.
 
-int main() {
+int main(){
     int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        vector<vector<int>> v(N, vector<int>(N));
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++) cin >> v[i][j];
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i = 0;i < n;i++)
+            cin>>a[i];
+        
         Solution ob;
-        v = ob.sortedMatrix(N, v);
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) cout << v[i][j] << " ";
-            cout << "\n";
-        }
+        vector<int> ans = ob.leftSmaller(n, a);
+        for(int i = 0;i < n;i++)
+            cout<<ans[i]<<" ";
+        cout<<endl;
     }
+    return 0;
 }  // } Driver Code Ends
