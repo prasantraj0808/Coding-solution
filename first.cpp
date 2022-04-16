847e7cb459c3d5e4e271a894f88d406c1aa68dec
Prasant <prasantraj0808@gmail.com>
2022-04-16 17:33:42 +0530

    maximum rectangular area in histogram

--- first.cpp
+++ first.cpp
@@ -1,65 +1,116 @@
-//https://practice.geeksforgeeks.org/problems/fab3dbbdce746976ba35c7b9b24afde40eae5a04/1/
+//https://practice.geeksforgeeks.org/problems/maximum-rectangular-area-in-a-histogram-1587115620/1#
 // { Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
 
  // } Driver Code Ends
-//User function Template for C++
-
-class Solution{
-    
+class Solution
+{
     public:
-    vector<int> help_classmate(vector<int> arr, int n) 
-    { 
-        // Your code goes here
-        vector<int > ans;
-        stack<int> st;
-        
-        for(int i=n-1;i>=0;i--)
-        { 
-            while(!st.empty() && st.top()>=arr[i])
+    //Function to find largest rectangular area possible in a given histogram.
+    long long getMaxArea(long long arr[], int n)
+    {
+        // Your code here
+        long long currentarea,maxiarea=INT_MIN;
+        long long nextsmaller[n];
+        long long previoussmaller[n];
+        stack<int >st;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && arr[st.top()]>=arr[i])
             {
                 st.pop();
             }
             if(st.empty())
-              ans.push_back(-1);
-              else
-              ans.push_back(st.top());
-              st.push(arr[i]);
+            previoussmaller[i]=-1;
+            else
+            previoussmaller[i]=st.top();
+            st.push(i);
+        }
+        /*
+        cout<<"printing previous smaller array"<<endl;
+        for(int i=0;i<n;i++)
+        {
+            cout<<previoussmaller[i]<<"  ";
             
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
-    } 
+        cout<<endl;
+        */
+        
+        while(!st.empty())
+        st.pop();
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            
+            while(!st.empty() && arr[st.top()]>=arr[i])
+            {
+                st.pop();
+            }
+            if(st.empty())
+            nextsmaller[i]=n;
+            else
+            nextsmaller[i]=st.top();
+            st.push(i);
+        }
+        int start=0,end=n-1,temp;
+        /*
+        while(start<end)
+        {
+            temp=nextsmaller[start];
+            nextsmaller[start]=nextsmaller[end];
+            nextsmaller[end]=temp;
+            start++;
+            end--;
+            
+        }
+        */
+        //reverse(nextsmaller,nextsmaller+n);
+        /*
+         cout<<"printing next smaller array"<<endl;
+        for(int i=0;i<n;i++)
+        {
+            cout<<nextsmaller[i]<<"  ";
+            
+        }
+        cout<<endl;
+        */
+        for(int i=0;i<n;i++)
+        {
+            //if(nextsmaller[i]==n)
+            currentarea=(nextsmaller[i]-previoussmaller[i]-1)*arr[i];
+            /*
+            else
+             currentarea=(nextsmaller[i]-previoussmaller[i]-1)*arr[i];
+             */
+            if(maxiarea<currentarea)
+            maxiarea=currentarea;
+        }
+        return maxiarea;
+    }
 };
+
 
 // { Driver Code Starts.
 
-int main() 
-{ 
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int n;
-		cin>>n;
-		vector<int> array(n);
-		for (int i = 0; i < n; ++i)
-		{
-			cin>>array[i];
-		}
-		Solution obj;
-		vector<int> result = obj.help_classmate(array,n);
-		for (int i = 0; i < n; ++i)
-		{
-			cout<<result[i]<<" ";
-		}
-		cout<<"\n";
-	}
-	return 0; 
-}
+int main()
+ {
+    long long t;
 
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        long long arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        cout<<ob.getMaxArea(arr, n)<<endl;
+    
+    }
+	return 0;
+}
   // } Driver Code Ends
