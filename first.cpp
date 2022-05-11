3215ba96c7e72319d86ed7e4c5e7821ca5809fd7
Prasant <prasantraj0808@gmail.com>
2022-05-11 14:14:07 +0530

    edit distance using tabulation

--- first.cpp
+++ first.cpp
@@ -6,23 +6,28 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-  int f(int i,int j,string &s1,string &s2,vector<vector<int>>&dp)
-  {
-      if(i<0) return j+1;
-      if(j<0) return i+1;
-      if(dp[i][j]!=-1) return dp[i][j];
-      if(s1[i]==s2[j])
-      return dp[i][j]=f(i-1,j-1,s1,s2,dp);
-      return dp[i][j]=1+min(f(i-1,j,s1,s2,dp),min(f(i,j-1,s1,s2,dp),f(i-1,j-1,s1,s2,dp)));  //delete  insert replace 
-      
-  }
-    int editDistance(string s, string t) {
+ 
+    int editDistance(string s1, string s2) {
         // Code here
-        int n=s.size(),m=t.size();
+        int n=s1.size(),m=s2.size();
         
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        int ans=f(n-1,m-1,s,t,dp);
-        return ans;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+       for(int j=0;j<=m;j++) dp[0][j]=j;
+       for(int i=0;i<=n;i++) dp[i][0]=i;
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=1;j<=m;j++)
+           {
+        if(s1[i-1]==s2[j-1])
+      dp[i][j]=dp[i-1][j-1];
+      else
+      {
+       dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));  //delete  insert replace 
+      }
+           }
+       }
+       return dp[n][m];
+       
         
     }
 };
