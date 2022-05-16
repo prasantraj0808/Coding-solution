c951267f5309e82fe99d4a63650dc7d410d08ba8
Prasant <prasantraj0808@gmail.com>
2022-05-16 10:04:32 +0530

    count palindromic substring

--- first.cpp
+++ first.cpp
@@ -1,34 +1,71 @@
-//https://leetcode.com/problems/longest-common-prefix/submissions/
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string ans="";
-        int min=INT_MAX;
-        for(auto x:strs)
-        {
-            if(min>x.length())
-            {
-                min=x.length();
-            }
-        }
-        int flag=0;
-        char currentchar;
-        string currentstring;
-        for(int i=0;i<min;i++)
-        {
-            currentstring=strs[0];
-            currentchar=currentstring[i];
-            for(auto x :strs)
-            {
-                if(x[i]!=currentchar)
-                {
-                    flag=1;
-                    return ans;
-                }
-            }
-            ans=ans+currentchar;
-        }
-        
-       return ans; 
+// https://practice.geeksforgeeks.org/problems/count-palindrome-sub-strings-of-a-string0652/1/#
+//https://www.youtube.com/watch?v=pp8K5C2hMr4
+#include<bits/stdc++.h>
+using namespace std;
+
+int CountPS(char S[], int N);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        char S[N+1];
+        cin>>S;
+        cout<<CountPS(S,N)<<endl;
     }
-};
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+int CountPS(char s[], int n)
+{
+    //code here
+   bool dp[n][n];
+   int ans=0;
+   for(int i=0;i<n;i++)
+   {
+       dp[i][i]=true;
+      // ans++;
+   }
+  // cout<<"two length"<<endl;
+   for(int i=0;i<n-1;i++)
+   {
+       if(s[i]==s[i+1])
+       {
+       dp[i][i+1]=true;
+      // cout<<i<<"----->"<<i+1<<endl;
+       ans++;
+       }
+       else
+       dp[i][i+1]=false;
+       
+   }
+  
+   for(int len=2;len<=n;len++)
+   {
+       for(int i=0;i+len<n;i++)
+       {
+           if(s[i]==s[i+len])
+           {
+               if(dp[i+1][i+len-1]==true)
+               {
+               dp[i][i+len]=true;
+               ans++;
+               }
+               else
+               dp[i][i+len]=false;
+           }
+           else
+           dp[i][i+len]=false;
+       }
+   }
+   
+    return ans;
+    
+}
